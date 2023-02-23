bb337b346e84fff289e29bf3242e9c52829b147a
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2023-02-23 16:25:55 +0100

    test1

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,35 @@
+import type { HeadersFunction, LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const headers: HeadersFunction = ({ actionHeaders, loaderHeaders }) => {
+  let headers = new Headers(actionHeaders);
+  loaderHeaders.has("Cache-Control") &&
+    headers.set("Cache-Control", loaderHeaders.get("Cache-Control")!);
+  return headers;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const date = new Date().toISOString();
+  return json(
+    {
+      date,
+      stuff: "stuff",
+    },
+    {
+      headers: {
+        "Cache-Control": "public, max-age=60",
+      },
+    }
+  );
+};
+
 export default function Index() {
+  let { date, stuff } = useLoaderData();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <p>Date: {date}</p>
+      <p>Stuff: {stuff}</p>
+    </>
   );
 }
